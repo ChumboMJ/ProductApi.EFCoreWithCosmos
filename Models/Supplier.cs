6a3ecab57b287366fa8fa0cceba373ab3ee32fc6
tim-forste <tforste@gmail.com>
2024-11-13 14:44:49 -0800

    Fill in the attibutes for Supplier Model

--- Models/Supplier.cs
+++ Models/Supplier.cs
@@ -2,5 +2,9 @@
 {
     public class Supplier
     {
+        public Guid SupplierId { get; set; }
+        public Guid ProductId { get; set; }
+        public required string Name { get; set; }
+        public required string ContactEmail { get; set; }
     }
 }
