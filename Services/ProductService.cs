ea862a31d9bb14ab4d848696f8e29afa6bd9727a
tim-forste <tforste@gmail.com>
2024-11-13 14:54:43 -0800

    Create ProductService, implementing IProductService - All methods throw NotImplementedException() currently

---
+++ Services/ProductService.cs
@@ -0,0 +1,28 @@
+﻿using ProductApi.EFCoreWithCosmos.Interfaces;
+using ProductApi.EFCoreWithCosmos.Models;
+
+namespace ProductApi.EFCoreWithCosmos.Services
+{
+    public class ProductService : IProductService
+    {
+        public Task<Product> Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(Guid productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product?> GetById(Guid productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product?> Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
