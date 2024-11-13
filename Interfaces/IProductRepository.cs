f05e04ae11fca75d6ab1e49cb29e42a885584287
tim-forste <tforste@gmail.com>
2024-11-13 15:19:09 -0800

    Remove redundant IProductRepository block

--- Interfaces/IProductRepository.cs
+++ Interfaces/IProductRepository.cs
@@ -4,12 +4,9 @@ namespace ProductApi.EFCoreWithCosmos.Interfaces
 {
     public interface IProductRepository
     {
-        public interface IProductRepository
-        {
-            Task Add(Product product);
-            Task<Product?> GetById(Guid productId);
-            Task<Product?> Update(Product product);
-            Task<bool> Delete(Guid productId);
-        }
+        Task Add(Product product);
+        Task<Product?> GetById(Guid productId);
+        Task<Product?> Update(Product product);
+        Task<bool> Delete(Guid productId);
     }
 }
