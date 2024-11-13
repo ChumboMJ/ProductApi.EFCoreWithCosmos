8cc60d9aafc30f93085c8ca25b68f77118735870
tim-forste <tforste@gmail.com>
2024-11-13 14:46:51 -0800

    Fill in attributes for Dimensions model

--- Models/Dimensions.cs
+++ Models/Dimensions.cs
@@ -2,5 +2,8 @@
 {
     public class Dimensions
     {
+        public required string Height { get; set; }
+        public required string Width { get; set; }
+        public required string Depth { get; set; }
     }
 }
