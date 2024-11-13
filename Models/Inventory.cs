1a5d86e97106e4ce931feb08bbce9fe5bad96387
tim-forste <tforste@gmail.com>
2024-11-13 14:43:20 -0800

    Fill in Inventory Model Attributes

--- Models/Inventory.cs
+++ Models/Inventory.cs
@@ -2,5 +2,9 @@
 {
     public class Inventory
     {
+        public Guid InventoryId { get; set; }
+        public Guid ProductId { get; set; }
+        public int StockQuantity { get; set; }
+        public DateTime LastRestocked { get; set; }
     }
 }
