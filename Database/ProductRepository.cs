09936f4a091611a33f4bd9dd44c870ed46aa805f
tim-forste <tforste@gmail.com>
2024-11-13 16:40:34 -0800

    Create a Function for Loading a Product along with its references

--- Database/ProductRepository.cs
+++ Database/ProductRepository.cs
@@ -6,16 +6,18 @@ namespace ProductApi.EFCoreWithCosmos.Database
 {
     public class ProductRepository : IProductRepository
     {
-        private readonly CosmosDbContext _context;
+        private readonly CosmosDbContext _dbContext;
 
         public ProductRepository(CosmosDbContext dbContext)
         {
-            _context = dbContext;
+            _dbContext = dbContext;
         }
 
-        public Task Add(Product product)
+        public async Task Add(Product product)
         {
-            throw new NotImplementedException();
+            _dbContext.Add(product);
+
+            await _dbContext.SaveChangesAsync();
         }
 
         public Task<bool> Delete(Guid productId)
@@ -23,9 +25,32 @@ namespace ProductApi.EFCoreWithCosmos.Database
             throw new NotImplementedException();
         }
 
-        public Task<Product?> GetById(Guid productId)
+        public async Task<Product?> GetById(Guid productId)
         {
-            throw new NotImplementedException();
+            return await LoadProductWithReferences(productId);
+        }
+
+        private async Task<Product?> LoadProductWithReferences(Guid productId)
+        {
+            var product = await _dbContext
+                .Products
+                .FindAsync(productId);
+
+            if (product == null) return null;
+
+            var productEntry = _dbContext.Products.Entry(product);
+
+            //Include Inventory from Inventory container
+            await productEntry
+                .Reference(p => p.Inventory)
+                .LoadAsync();
+
+            //Include Supplier from Suppliers container
+            await productEntry
+                .Reference(p => p.Suppliers)
+                .LoadAsync();
+
+            return product;
         }
 
         public Task<Product?> Update(Product product)
