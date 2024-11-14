d5026f463b6e5362c3ae31e55ab4fb5293551ab0
tim-forste <tforste@gmail.com>
2024-11-13 16:24:31 -0800

    Begin configuring DbContext

---
+++ Database/CosmosDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ProductApi.EFCoreWithCosmos.Models;
+
+namespace ProductApi.EFCoreWithCosmos.Database
+{
+    public class CosmosDbContext : DbContext
+    {
+        public CosmosDbContext(DbContextOptions options) : base(options) {  }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Supplier> Suppliers { get; set; }
+        public DbSet<Inventory> Inventory { get; set; }
+
+        override protected void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAutoscaleThroughput(1000);
+            modelBuilder.HasDefaultContainer("Products");
+        }
+    }
+}
