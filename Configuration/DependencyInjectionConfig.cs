98f7d2c9e5e75b2aa22e088702a17aec78592e6e
tim-forste <tforste@gmail.com>
2024-11-14 14:04:18 -0800

    Complete DI Configuration

---
+++ Configuration/DependencyInjectionConfig.cs
@@ -0,0 +1,20 @@
+﻿using ProductApi.EFCoreWithCosmos.Database;
+using ProductApi.EFCoreWithCosmos.Interfaces;
+using ProductApi.EFCoreWithCosmos.Services;
+
+namespace CosmosDbEfCoreDemo.API.Configuration
+{
+    public static class DependencyInjectionConfig
+    {
+        public static IServiceCollection ResolveDependencies(this IServiceCollection services)
+        {
+            services.AddScoped<CosmosDbContext>();
+
+            services.AddScoped<IProductService, ProductService>();
+
+            services.AddScoped<IProductRepository, ProductRepository>();
+
+            return services;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,9 @@
 
+using CosmosDbEfCoreDemo.API.Configuration;
+using Microsoft.EntityFrameworkCore;
+using ProductApi.EFCoreWithCosmos.Database;
+using System.Configuration;
+
 namespace ProductApi.EFCoreWithCosmos
 {
     public class Program
@@ -8,11 +13,24 @@ namespace ProductApi.EFCoreWithCosmos
             var builder = WebApplication.CreateBuilder(args);
 
             // Add services to the container.
+            builder.Services.AddDbContextFactory<CosmosDbContext>(optionsBuilder =>
+              optionsBuilder
+                .UseCosmos(
+                  connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
+                  databaseName: "SampleInventory",
+                  cosmosOptionsAction: options =>
+                  {
+                      options.ConnectionMode(Microsoft.Azure.Cosmos.ConnectionMode.Direct);
+                      options.MaxRequestsPerTcpConnection(16);
+                      options.MaxTcpConnectionsPerEndpoint(32);
+                  }));
 
             builder.Services.AddControllers();
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+
+            builder.Services.ResolveDependencies();
 
             var app = builder.Build();
 
