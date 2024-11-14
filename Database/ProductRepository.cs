b309a64ce515acd1a0e70117a17f2d7b68937af6
tim-forste <tforste@gmail.com>
2024-11-13 16:32:15 -0800

    Create shell for Product Repository

---
+++ Database/ProductRepository.cs
@@ -0,0 +1,28 @@
+﻿using ProductApi.EFCoreWithCosmos.Interfaces;
+using ProductApi.EFCoreWithCosmos.Models;
+
+namespace ProductApi.EFCoreWithCosmos.Database
+{
+    public class ProductRepository : IProductRepository
+    {
+        public Task Add(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> Delete(Guid productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product?> GetById(Guid productId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<Product?> Update(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
