1430369e4f2cba14b47c42222e90b55ea5fbe144
ChumboMJ <tforste@gmail.com>
2024-12-15 14:14:00 -0800

    Update Product Service Method Naming to Reflect Async Functions

--- Interfaces/IProductService.cs
+++ Interfaces/IProductService.cs
@@ -4,9 +4,9 @@ namespace ProductApi.EFCoreWithCosmos.Interfaces
 {
     public interface IProductService
     {
-        Task<Product> Add(Product product);
-        Task<Product?> GetById(Guid productId);
-        Task<Product?> Update(Product product);
-        Task<bool> Delete(Guid productId);
+        Task<Product> AddProductAsync(Product product);
+        Task<Product?> GetProductByIdAsync(Guid productId);
+        Task<Product?> UpdateProductAsync(Product product);
+        Task<bool> DeleteProductAsync(Guid productId);
     }
 }
