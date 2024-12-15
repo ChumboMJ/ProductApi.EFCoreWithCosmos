b559b5e44c85a7d72025ccbd87db86bf63eca78b
ChumboMJ <tforste@gmail.com>
2024-12-14 19:34:04 -0800

    Format the DI config and add apollographql to Cors to allow use

--- Configuration/DependencyInjectionConfig.cs
+++ Configuration/DependencyInjectionConfig.cs
@@ -1,5 +1,8 @@
-﻿using ProductApi.EFCoreWithCosmos.Database;
+﻿using GraphQL.Types;
+using ProductApi.EFCoreWithCosmos.Database;
+using ProductApi.EFCoreWithCosmos.GraphQL;
 using ProductApi.EFCoreWithCosmos.Interfaces;
+using ProductApi.EFCoreWithCosmos.Models;
 using ProductApi.EFCoreWithCosmos.Services;
 
 namespace CosmosDbEfCoreDemo.API.Configuration
@@ -9,10 +12,21 @@ namespace CosmosDbEfCoreDemo.API.Configuration
         public static IServiceCollection ResolveDependencies(this IServiceCollection services)
         {
             services.AddScoped<CosmosDbContext>();
-
             services.AddScoped<IProductService, ProductService>();
-
             services.AddScoped<IProductRepository, ProductRepository>();
+
+            //Totorial: https://www.apollographql.com/tutorials/intro-hotchocolate/05-apollo-explorer
+            //Allow apollo to be used
+            services.AddCors(options =>
+                {
+                    options.AddDefaultPolicy(builder =>
+                    {
+                        builder
+                            .WithOrigins("https://studio.apollographql.com")
+                            .AllowAnyHeader()
+                            .AllowAnyMethod();
+                    });
+                });
 
             return services;
         }
