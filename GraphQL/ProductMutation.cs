b0a9aa69cc6023d7b0381e7b44eee128cf7b5af1
ChumboMJ <tforste@gmail.com>
2024-12-15 17:43:02 -0800

    Create initial ProductMutation class

---
+++ GraphQL/ProductMutation.cs
@@ -0,0 +1,21 @@
+﻿using ProductApi.EFCoreWithCosmos.Interfaces;
+using ProductApi.EFCoreWithCosmos.Models;
+
+namespace ProductApi.EFCoreWithCosmos.GraphQL
+{
+    public class ProductMutation
+    {
+        public async Task<Product> AddProductAsync(string name, string category, Dimensions dimensions,
+            [Service] IProductService productService)
+        {
+            var product = new Product(Guid.NewGuid())
+            {
+                Category = category,
+                Name = name,
+                Dimensions = dimensions
+            };
+
+            return await productService.AddProductAsync(product);
+        }
+    }
+}
