e275cd35fd8c6d89602bcacbc2a488962edf0215
ChumboMJ <tforste@gmail.com>
2024-12-17 13:36:25 -0800

    Adding a new ProductApiOptions object to allow for implementation of Options Pattern in the Program configuration

---
+++ Options/ProductApiOptions.cs
@@ -0,0 +1,15 @@
+﻿namespace ProductApi.EFCoreWithCosmos
+{
+    public class ProductApiOptions
+    {
+        public const string ProductApi = "ProductApi";
+        public string AllowedHosts { get; set; }
+        public CosmosDbOptions CosmosDb { get; set; }
+
+        public class CosmosDbOptions
+        {
+            public string ConnectionString { get; set; }
+            public string DatabaseName { get; set; }
+        }
+    }
+}
