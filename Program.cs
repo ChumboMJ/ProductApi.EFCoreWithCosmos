f65c9976415f13846c0d5156abfa411be0593df9
ChumboMJ <tforste@gmail.com>
2024-12-17 13:13:24 -0800

    Update Program.cs formatting and add function to configure AzureKeyVault

--- Program.cs
+++ Program.cs
@@ -1,19 +1,17 @@
 
+using Azure.Identity;
 using CosmosDbEfCoreDemo.API.Configuration;
 using Microsoft.EntityFrameworkCore;
 using ProductApi.EFCoreWithCosmos.Database;
 using ProductApi.EFCoreWithCosmos.GraphQL;
+using System.Security.Cryptography.X509Certificates;
 
-namespace ProductApi.EFCoreWithCosmos
-{
-    public class Program
-    {
-        public static void Main(string[] args)
-        {
-            var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-            // Add services to the container.
-            builder.Services.AddDbContextFactory<CosmosDbContext>(optionsBuilder =>
+ConfigureKeyVault(builder);
+
+// Add services to the container.
+builder.Services.AddDbContextFactory<CosmosDbContext>(optionsBuilder =>
               optionsBuilder
                 .UseCosmos(
                   connectionString: builder.Configuration.GetConnectionString("DefaultConnection"),
@@ -25,36 +23,62 @@ namespace ProductApi.EFCoreWithCosmos
                       options.MaxTcpConnectionsPerEndpoint(32);
                   }));
 
-            builder.Services.AddControllers();
-            // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
-            builder.Services.AddEndpointsApiExplorer();
-            builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
-            //Configure GraphQL
-            builder.Services.AddGraphQLServer()
+//Configure GraphQL
+builder.Services.AddGraphQLServer()
                 .AddQueryType<ProductQuery>()
                 .AddMutationType<ProductMutation>();
 
-            builder.Services.ResolveDependencies();
+builder.Services.ResolveDependencies();
 
-            var app = builder.Build();
+var app = builder.Build();
 
-            // Configure the HTTP request pipeline.
-            if (app.Environment.IsDevelopment())
-            {
-                app.UseSwagger();
-                app.UseSwaggerUI();
-            }
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
-            app.UseHttpsRedirection();
+app.UseHttpsRedirection();
 
-            app.UseCors();
-            app.UseAuthorization();
+app.UseCors();
+app.UseAuthorization();
 
-            app.MapGraphQL();
-            app.MapControllers();
+app.MapGraphQL();
+app.MapControllers();
 
-            app.Run();
-        }
+app.Run();
+        
+
+/// <summary>
+/// Set up Azure Key Vault if a KeyVault name is provided in the configuration
+/// </summary>
+/// <param name="builder">WebApplicationBuilder</param>
+void ConfigureKeyVault(WebApplicationBuilder builder)
+{
+    if (!string.IsNullOrEmpty(builder.Configuration.GetSection("KeyVault")["Name"]))
+    {
+        using var x509Store = new X509Store(StoreLocation.LocalMachine);
+        x509Store.Open(OpenFlags.ReadOnly);
+
+        var x509Certificate = x509Store.Certificates
+            .Find(
+                X509FindType.FindByThumbprint,
+                builder.Configuration.GetSection("KeyVault").GetSection("AzureAd")["CertificateThumbprint"],
+                validOnly: false)
+            .OfType<X509Certificate2>()
+            .Single();
+
+        builder.Configuration.AddAzureKeyVault(
+            new Uri($"https://{builder.Configuration.GetSection("KeyVault")["Name"]}.vault.azure.net/"),
+            new ClientCertificateCredential(
+                    builder.Configuration.GetSection("KeyVault").GetSection("AzureAd")["DirectoryId"],
+                    builder.Configuration.GetSection("KeyVault").GetSection("AzureAd")["ApplicationId"],
+                    x509Certificate));
     }
 }
